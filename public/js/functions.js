fd9840599ae13e6eb0b366f700c139ad3a6cc5f8
marcosrabelocod <marcosrabelocod@gmail.com>
2025-11-20 14:52:32 -0300

    refresh

--- public/js/functions.js
+++
@@ -1,8 +0,0 @@
-var lon = document.getElementById('longitude').innerText
-var lat = document.getElementById('latitude').innerText
-const mapa = document.getElementById('mapa');
-
-// Adiciona o evento de duplo clique
-mapa.addEventListener('dblclick', () => {
-    window.location.href = '/mapa/' + lat + '/' + lon; 
-});
--- public/js/webmap.js
+++
@@ -1,5 +0,0 @@
-var map = L.map('map').setView([-1.474560, -48.452322], 17);
-L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    maxZoom: 19,
-    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-}).addTo(map);
